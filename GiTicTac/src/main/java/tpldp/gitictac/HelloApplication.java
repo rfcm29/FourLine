1050ff7c34a43dc1bed42ed3ebe8177c6af0920a
Ivo Andrade <IRPAndrade@gmail.com>
2024-06-22 17:49:55 +0100

    teste 1

--- GiTicTac/src/main/java/tpldp/gitictac/HelloApplication.java
+++ GiTicTac/src/main/java/tpldp/gitictac/HelloApplication.java
@@ -15,6 +15,8 @@ public class HelloApplication extends Application {
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
+
+        /* ola */
     }
 
     public static void main(String[] args) {
